479f141be961231b6e98a1ab43b59a1ae929b418
Gabrieli Arpini <senac.gabrieliarpini@gmail.com>
2025-03-10 19:16:41 -0300

    feat: adiciona calculo de imc de adultos homens

---
+++ MultApps/MultApp.Windows/frmCalculadoraIMC.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace MultApp.Windows
+{
+    partial class frmCalculadoraIMC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblImc = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbIdade = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblImcAdulto = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.chkMasc = new System.Windows.Forms.CheckBox();
+            this.chkFem = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkCrianca = new System.Windows.Forms.CheckBox();
+            this.chkAdulto = new System.Windows.Forms.CheckBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblResultado);
+            this.panel1.Controls.Add(this.btnCalcular);
+            this.panel1.Controls.Add(this.lblImc);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.cmbIdade);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtAltura);
+            this.panel1.Controls.Add(this.txtPeso);
+            this.panel1.Controls.Add(this.maskedTextBox1);
+            this.panel1.Controls.Add(this.lblImcAdulto);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblIdade);
+            this.panel1.Controls.Add(this.chkMasc);
+            this.panel1.Controls.Add(this.chkFem);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.chkCrianca);
+            this.panel1.Controls.Add(this.chkAdulto);
+            this.panel1.Controls.Add(this.lbl1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnCalcular.Location = new System.Drawing.Point(67, 267);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(122, 35);
+            this.btnCalcular.TabIndex = 19;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblImc
+            // 
+            this.lblImc.AutoSize = true;
+            this.lblImc.BackColor = System.Drawing.Color.Transparent;
+            this.lblImc.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImc.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblImc.Location = new System.Drawing.Point(28, 323);
+            this.lblImc.Name = "lblImc";
+            this.lblImc.Size = new System.Drawing.Size(123, 27);
+            this.lblImc.TabIndex = 18;
+            this.lblImc.Text = "Seu IMC é: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Location = new System.Drawing.Point(294, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 14);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Idade ";
+            // 
+            // cmbIdade
+            // 
+            this.cmbIdade.FormattingEnabled = true;
+            this.cmbIdade.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19"});
+            this.cmbIdade.Location = new System.Drawing.Point(297, 231);
+            this.cmbIdade.Name = "cmbIdade";
+            this.cmbIdade.Size = new System.Drawing.Size(121, 21);
+            this.cmbIdade.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Location = new System.Drawing.Point(140, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 14);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Altura (M)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(23, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 14);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Peso (KG)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(143, 232);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(124, 20);
+            this.txtAltura.TabIndex = 11;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(21, 232);
+            this.txtPeso.Multiline = true;
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 10;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 9;
+            // 
+            // lblImcAdulto
+            // 
+            this.lblImcAdulto.AutoSize = true;
+            this.lblImcAdulto.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImcAdulto.Location = new System.Drawing.Point(18, 191);
+            this.lblImcAdulto.Name = "lblImcAdulto";
+            this.lblImcAdulto.Size = new System.Drawing.Size(132, 19);
+            this.lblImcAdulto.TabIndex = 8;
+            this.lblImcAdulto.Text = "IMC para Adulto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(16, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Faixa Etária";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblIdade.Location = new System.Drawing.Point(19, 154);
+            this.lblIdade.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(103, 16);
+            this.lblIdade.TabIndex = 6;
+            this.lblIdade.Text = "Acima de 19 anos";
+            // 
+            // chkMasc
+            // 
+            this.chkMasc.AutoSize = true;
+            this.chkMasc.BackColor = System.Drawing.Color.Transparent;
+            this.chkMasc.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMasc.ForeColor = System.Drawing.Color.DimGray;
+            this.chkMasc.Location = new System.Drawing.Point(449, 107);
+            this.chkMasc.Name = "chkMasc";
+            this.chkMasc.Size = new System.Drawing.Size(129, 33);
+            this.chkMasc.TabIndex = 5;
+            this.chkMasc.Text = "Masculino";
+            this.chkMasc.UseVisualStyleBackColor = false;
+            this.chkMasc.CheckedChanged += new System.EventHandler(this.chkMasc_CheckedChanged);
+            // 
+            // chkFem
+            // 
+            this.chkFem.AutoSize = true;
+            this.chkFem.Checked = true;
+            this.chkFem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFem.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkFem.ForeColor = System.Drawing.Color.DarkOrange;
+            this.chkFem.Location = new System.Drawing.Point(322, 107);
+            this.chkFem.Name = "chkFem";
+            this.chkFem.Size = new System.Drawing.Size(121, 33);
+            this.chkFem.TabIndex = 4;
+            this.chkFem.Text = "Feminino";
+            this.chkFem.UseVisualStyleBackColor = true;
+            this.chkFem.CheckedChanged += new System.EventHandler(this.chkFem_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(318, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Gênero";
+            // 
+            // chkCrianca
+            // 
+            this.chkCrianca.AutoSize = true;
+            this.chkCrianca.BackColor = System.Drawing.Color.Transparent;
+            this.chkCrianca.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkCrianca.ForeColor = System.Drawing.Color.DimGray;
+            this.chkCrianca.Location = new System.Drawing.Point(116, 107);
+            this.chkCrianca.Name = "chkCrianca";
+            this.chkCrianca.Size = new System.Drawing.Size(103, 33);
+            this.chkCrianca.TabIndex = 2;
+            this.chkCrianca.Text = "Criança";
+            this.chkCrianca.UseVisualStyleBackColor = false;
+            this.chkCrianca.Click += new System.EventHandler(this.chkCrianca_CheckedChanged);
+            // 
+            // chkAdulto
+            // 
+            this.chkAdulto.AutoSize = true;
+            this.chkAdulto.Checked = true;
+            this.chkAdulto.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAdulto.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAdulto.ForeColor = System.Drawing.Color.DarkOrange;
+            this.chkAdulto.Location = new System.Drawing.Point(20, 107);
+            this.chkAdulto.Name = "chkAdulto";
+            this.chkAdulto.Size = new System.Drawing.Size(96, 33);
+            this.chkAdulto.TabIndex = 1;
+            this.chkAdulto.Text = "Adulto";
+            this.chkAdulto.UseVisualStyleBackColor = true;
+            this.chkAdulto.Click += new System.EventHandler(this.chkAdulto_CheckedChanged);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Tai Le", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl1.Location = new System.Drawing.Point(16, 18);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(150, 39);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Selecione";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.Color.Transparent;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Black;
+            this.lblResultado.Location = new System.Drawing.Point(151, 323);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 24);
+            this.lblResultado.TabIndex = 20;
+            // 
+            // frmCalculadoraIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmCalculadoraIMC";
+            this.Text = "frmCalculadoraIMC";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkAdulto;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.CheckBox chkCrianca;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkFem;
+        private System.Windows.Forms.CheckBox chkMasc;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label lblImcAdulto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.ComboBox cmbIdade;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblImc;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ MultApps/MultApp.Windows/frmCalculadoraIMC.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApp.Windows
+{
+    public partial class frmCalculadoraIMC : Form
+    {
+        public frmCalculadoraIMC()
+        {
+            InitializeComponent();
+        }
+
+        private void chkCrianca_CheckedChanged(object sender, EventArgs e)
+        {
+            chkCrianca.ForeColor = Color.DarkOrange;
+            chkAdulto.ForeColor = Color.DimGray;
+            chkAdulto.Checked = false;
+
+            lblIdade.Text = "Abaixo dos 19";
+            lblImcAdulto.Text = "Z IMC para Criança";
+
+            label6.Visible = true;
+            cmbIdade.Visible = true;
+
+
+
+        }
+
+        private void chkAdulto_CheckedChanged(object sender, EventArgs e)
+        {
+            chkCrianca.ForeColor = Color.DimGray;
+            chkAdulto.ForeColor = Color.DarkOrange;
+            chkCrianca.Checked = false;
+            lblIdade.Text = "Acima dos 19";
+
+            lblImcAdulto.Text = "IMC para Adulto";
+
+            label6.Visible = false;
+            cmbIdade.Visible = false;
+        }
+
+        private void chkMasc_CheckedChanged(object sender, EventArgs e)
+        {
+            chkMasc.ForeColor = Color.DarkOrange;
+            chkFem.ForeColor = Color.DimGray;
+            chkFem.Checked = false;
+
+        }
+
+        private void chkFem_CheckedChanged(object sender, EventArgs e)
+        {
+            chkFem.ForeColor = Color.DarkOrange;
+            chkMasc.ForeColor = Color.DimGray;
+            chkMasc.Checked = false;
+
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            var peso = double.Parse(txtPeso.Text);
+            var altura = double.Parse(txtAltura.Text);
+
+            var imc = peso / (altura * altura);
+            var textoBase = $@"Meu IMC: {imc:N2} é";
+
+            if (imc <= 18.5)
+            {
+
+                lblResultado.Text = $@"{textoBase} abaixo do normal.";
+            }
+
+            else if (imc < 24.9)
+            {
+                lblResultado.Text = $@"{textoBase} normal.";
+            }
+
+            else if (imc < 29.9)
+            {
+                lblResultado.Text = $@"{textoBase} sobrepeso.";
+            }
+
+            else if (imc < 34.9)
+            {
+                lblResultado.Text = $@"{textoBase} obesidade grau 1.";
+            }
+
+            else if (imc < 39.9)
+            {
+                lblResultado.Text = $@"{textoBase} obesidade grau 2.";
+            }
+
+            else 
+            {
+                lblResultado.Text = $@"{textoBase} é obesidade grau 3.";
+            }
+
+        }
+    }
+}
+
